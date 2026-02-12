d73afe831766d0db06980eb8d02bc199bda99ec7
speech-ka <speech-ka@tutamail.com>
2026-02-12 08:05:19 +0300

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,81 @@
 use anyhow::Result;
-use wasmtime::component::*;
+use wasmtime::component::{
+	bindgen, Component, HasSelf, Linker, Resource as WasmtimeResource, ResourceTable,
+};
 use wasmtime::{Config, Engine, Store};
-use bevy::prelude::*;
+
+use crate::fallow::midden::krenel::{Host as KrenelHost, HostActor, Point};
+use crate::wasi::logging::logging::{Host as WasiLoggingHost, Level as WasiLogLevel};
 
 bindgen!({
-	world: "calculator",
-	path: "./moonbit-guest/wit",
+    path: "moonbit-guest/wit",
+    world: "fortalice",
+    with: {
+        "fallow:midden/krenel.actor": Actor,
+    },
+
+	//imports: { default: trappable },
 });
 
-struct HostState;
-
-fn main() -> AppExit {
-	App::new()
-		.add_systems(PreStartup, setup_wasm_engine)
-		.run()
+#[derive(Clone)]
+pub struct Actor {
+	pos: fallow::midden::krenel::Point,
+	kind: String,
 }
 
-fn setup_wasm_engine(mut commands: Commands) {
-	//
+#[derive(Default)]
+struct HostState {
+	table: ResourceTable,
 }
 
-fn idk() -> Result<()> {
+impl HostActor for HostState {
+	fn get_position(&mut self, actor: WasmtimeResource<Actor>) -> Point {
+		let actor = self.table.get(&actor).unwrap();
+		actor.pos
+	}
+
+	fn set_position(&mut self, actor: WasmtimeResource<Actor>, pos: Point) -> () {
+		let actor = self.table.get_mut(&actor).unwrap();
+		actor.pos = pos;
+	}
+
+	fn drop(&mut self, actor: WasmtimeResource<Actor>) -> Result<()> {
+		let _actor: Actor = self.table.delete(actor)?;
+		Ok(())
+	}
+}
+
+impl KrenelHost for HostState {
+	fn spawn(&mut self, kind: String, pos: Point) -> WasmtimeResource<Actor> {
+		let actor = Actor { pos, kind };
+		self.table.push(actor).unwrap()
+	}
+}
+
+impl WasiLoggingHost for HostState {
+	fn log(&mut self, level: WasiLogLevel, context: String, message: String) {
+		match level {
+			WasiLogLevel::Trace => log::trace!("[{context}] {message}"),
+			WasiLogLevel::Debug => log::debug!("[{context}] {message}"),
+			WasiLogLevel::Info => log::info!("[{context}] {message}"),
+			WasiLogLevel::Warn => log::warn!("[{context}] {message}"),
+			WasiLogLevel::Error => log::error!("[{context}] {message}"),
+			WasiLogLevel::Critical => log::error!("[CRITICAL][{context}] {message}"),
+		}
+	}
+}
+
+fn main() -> Result<()> {
+	env_logger::init();
 	let mut config = Config::new();
 	config.wasm_component_model(true);
-
 	let engine = Engine::new(&config)?;
-	let linker = Linker::new(&engine);
-	let mut store = Store::new(&engine, HostState);
-
-	let component = Component::from_file(
-		&engine,
-		"./moonbit-guest/component.wasm"
-	)?;
-	let bindings = Calculator::instantiate(&mut store, &component, &linker)?;
-
-	let point_a = Point { x: 10, y: 20 };
-	let point_b = Point { x: 5, y: 15 };
-
-	println!("Point A: ({}, {})", point_a.x, point_a.y);
-	println!("Point B: ({}, {})", point_b.x, point_b.y);
-
-	let sum = bindings.call_add_points(&mut store, point_a, point_b)?;
-	println!("Sum: ({}, {})", sum.x, sum.y);
-
-	let result = bindings.call_calculate(&mut store, point_a, point_b)?;
-	println!("Calculation Result:");
-	println!("  Sum: ({}, {})", result.sum.x, result.sum.y);
-	println!("  Distance: {:.2}", result.distance);
-
-	let origin = bindings.call_get_origin(&mut store)?;
-	println!("Origin: ({}, {})", origin.x, origin.y);
+	let component = Component::from_file(&engine, "moonbit-guest/component.wasm")?;
+	let mut linker = Linker::<HostState>::new(&engine);
+	Fortalice::add_to_linker::<_, HasSelf<_>>(&mut linker, |state: &mut HostState| state)?;
+	let mut store = Store::new(&engine, HostState::default());
+	let bindings = Fortalice::instantiate(&mut store, &component, &linker)?;
+	bindings.call_on_update(&mut store, 1.0 / 60.0)?;
 
 	Ok(())
 }
